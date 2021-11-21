c830d286f8d2a23cf3bc245dcae48f6abd3826ad
tatev <tatev.igityan@bk.ru>
2021-11-21 20:53:29 +0300

    Updates

---
+++ precache-manifest.2a597effe857b8c4ba721e78775092bc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b2ed9554f7bf9bd886ee53627662766",
+    "url": "//tatevigityan.github.io/react-firebase-chat-master/index.html"
+  },
+  {
+    "revision": "41a399974d8e3a1fbbea",
+    "url": "//tatevigityan.github.io/react-firebase-chat-master/static/css/main.9cb7495d.chunk.css"
+  },
+  {
+    "revision": "e8069fb2bee25e00f463",
+    "url": "//tatevigityan.github.io/react-firebase-chat-master/static/js/2.68f4f41f.chunk.js"
+  },
+  {
+    "revision": "40f7fa7ab5996d1e53a916b11d456d6f",
+    "url": "//tatevigityan.github.io/react-firebase-chat-master/static/js/2.68f4f41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41a399974d8e3a1fbbea",
+    "url": "//tatevigityan.github.io/react-firebase-chat-master/static/js/main.0be366b3.chunk.js"
+  },
+  {
+    "revision": "5f367abb509663d51f58",
+    "url": "//tatevigityan.github.io/react-firebase-chat-master/static/js/runtime-main.f2a9c1c1.js"
+  }
+]);
